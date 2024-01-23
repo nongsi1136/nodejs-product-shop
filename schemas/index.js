c30b140243b9054c68ab1805e384e1ce81de43c3
sinhong jeong <jsh719803@gmail.com>
2024-01-23 22:07:14 +0900

    hide mongodb url

--- schemas/index.js
+++ schemas/index.js
@@ -1,21 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connect = () => {
   // mongoose.connect는 MongoDB 서버에 연결하는 메서드입니다.
+  const dbUri = process.env.MONGODB_URI;
   mongoose
     .connect(
       // 빨간색으로 표시된 부분은 대여한 ID, Password, 주소에 맞게끔 수정해주세요!
-      "mongodb+srv://jsh719803:jn4023104512@express-mongo.u0kzxh8.mongodb.net/?retryWrites=true&w=majority",
+      // "mongodb+srv://jsh719803:jn4023104512@express-mongo.u0kzxh8.mongodb.net/?retryWrites=true&w=majority",
+      dbUri,
       {
-        dbName: "product_shop", // product_shop 데이터베이스명을 사용합니다.
+        dbName: 'product_shop', // product_shop 데이터베이스명을 사용합니다.
       }
     )
-    .then(() => console.log("MongoDB 연결에 성공하였습니다."))
+    .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
 
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB 연결 에러", err);
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB 연결 에러', err);
 });
 
 export default connect;
