a32fd12ce31dc3aced56abb5dd946f89e84f95a7
sinhong jeong <jsh719803@gmail.com>
2024-01-23 22:14:07 +0900

    Remove unnecessary comments

--- schemas/index.js
+++ schemas/index.js
@@ -7,14 +7,9 @@ const connect = () => {
   // mongoose.connect는 MongoDB 서버에 연결하는 메서드입니다.
   const dbUri = process.env.MONGODB_URI;
   mongoose
-    .connect(
-      // 빨간색으로 표시된 부분은 대여한 ID, Password, 주소에 맞게끔 수정해주세요!
-      // "mongodb+srv://jsh719803:jn4023104512@express-mongo.u0kzxh8.mongodb.net/?retryWrites=true&w=majority",
-      dbUri,
-      {
-        dbName: 'product_shop', // product_shop 데이터베이스명을 사용합니다.
-      }
-    )
+    .connect(dbUri, {
+      dbName: 'product_shop', // product_shop 데이터베이스명을 사용합니다.
+    })
     .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
     .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
 };
