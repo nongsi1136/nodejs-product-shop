04766bcc709816344986d9a68a25a48baadcc4ea
sinhong jeong <jsh719803@gmail.com>
2024-01-23 20:39:48 +0900

    Fix API error in product deletion

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,16 +1,16 @@
-import express from "express";
-import Product from "../schemas/products.schema.js";
+import express from 'express';
+import Product from '../schemas/products.schema.js';
 
 const router = express.Router();
-// 상품 작성 API //
-router.allpost("/products", async (req, res, next) => {
+// 1. 상품 작성 API //
+router.post('/products', async (req, res) => {
   const { productName, content, authorName, password } = req.body;
 
   // 데이터 유효성 검사 기능
   if (!productName || !content || !authorName || !password) {
     return res
       .status(400)
-      .json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
+      .json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
   }
 
   try {
@@ -19,7 +19,7 @@ router.allpost("/products", async (req, res, next) => {
       content,
       authorName,
       password,
-      status: "FOR_SALE",
+      status: 'FOR_SALE',
     });
 
     await newProduct.save();
@@ -28,49 +28,49 @@ router.allpost("/products", async (req, res, next) => {
   } catch (error) {
     return res
       .status(500)
-      .json({ errorMessage: "상품 등록에 실패하였습니다." });
+      .json({ errorMessage: '상품 등록에 실패하였습니다.' });
   }
 });
 
 // 2. 상품 목록 조회 API
-router.get("/products", async (req, res, next) => {
+router.get('/products', async (req, res) => {
   try {
     const products = await Product.find()
-      .sort({ createAt: -1 }) // 최신순으로 정렬
-      .select("productName authorName status creatAt"); //데이터를 조회할 때 반환할 필드를 선택하는 것
+      .sort({ createdAt: -1 }) // 최신순으로 정렬
+      .select('productName authorName status creatAt'); //데이터를 조회할 때 반환할 필드를 선택하는 것
 
     return res.status(200).json({ products });
   } catch (error) {
     return res
       .status(500)
-      .json({ errorMessage: "상품 목록 조회에 실패하였습니다." });
+      .json({ errorMessage: '상품 목록 조회에 실패하였습니다.' });
   }
 });
 
 //3. 상품 상세 조회 API
-router.get("/products/:productId", async (req, res, next) => {
+router.get('/products/:productId', async (req, res) => {
   const { productId } = req.params;
 
   try {
     const product = await Product.findById(productId).select(
-      "productName content authorName status createdAt"
+      'productName content authorName status createdAt'
     );
 
     if (!product) {
       return res
         .status(404)
-        .json({ errorMessage: "상품 조회에 실패하였습니다." });
+        .json({ errorMessage: '상품 조회에 실패하였습니다.' });
     }
     return res.status(200).json({ product });
   } catch (error) {
     return res
       .status(500)
-      .json({ errorMessage: "상품 상세 조회에 실패하였습니다." });
+      .json({ errorMessage: '상품 상세 조회에 실패하였습니다.' });
   }
 });
 
 // 4. 상품 정보 수정 API
-router.put("/products/:productId", async (req, res, next) => {
+router.put('/products/:productId', async (req, res) => {
   const { productId } = req.params;
   const { productName, content, status, password } = req.body;
 
@@ -78,7 +78,7 @@ router.put("/products/:productId", async (req, res, next) => {
     if (!productName || !content || !status || !password) {
       return res
         .status(400)
-        .json({ errorMessage: "데이터 형식이 올바르지 않습니다" });
+        .json({ errorMessage: '데이터 형식이 올바르지 않습니다' });
     }
 
     const product = await Product.findById(productId);
@@ -86,13 +86,13 @@ router.put("/products/:productId", async (req, res, next) => {
     if (!product) {
       return res
         .status(404)
-        .json({ errorMessage: "상품 조회에 실패하였습니다." });
+        .json({ errorMessage: '상품 조회에 실패하였습니다.' });
     }
 
     if (password !== product.password) {
       return res
         .status(401)
-        .json({ errorMessage: "상품을 수정할 권한이 존재하지 않습니다." });
+        .json({ errorMessage: '상품을 수정할 권한이 존재하지 않습니다.' });
     }
 
     product.productName = productName || product.productName;
@@ -103,11 +103,11 @@ router.put("/products/:productId", async (req, res, next) => {
 
     return res.status(200).json({ product });
   } catch (error) {
-    return res.status(500).json({ error: "상품 정보 수정에 실패하였습니다." });
+    return res.status(500).json({ error: '상품 정보 수정에 실패하였습니다.' });
   }
 });
 // 5. 상품 삭제 API
-router.delete("/product/:productId", async (req, res, next) => {
+router.delete('/products/:productId', async (req, res) => {
   const { productId } = req.params;
   const { password } = req.body;
 
@@ -117,19 +117,21 @@ router.delete("/product/:productId", async (req, res, next) => {
     if (!product) {
       return res
         .status(404)
-        .json({ errorMessage: "상품 조회에 실패하셨습니다." });
+        .json({ errorMessage: '상품 조회에 실패하셨습니다.' });
     }
 
     if (password !== product.password) {
       return res
         .status(401)
-        .json({ errorMessage: "상품을 삭제할 권한이 존재하지 않습니다." });
+        .json({ errorMessage: '상품을 삭제할 권한이 존재하지 않습니다.' });
     }
-    await product.remove();
+    await product.deleteOne();
 
     return res.status(200).json({});
   } catch (error) {
-    return res.status(500).json({ error: "상품 삭제에 실패하였습니다." });
+    return res
+      .status(500)
+      .json({ errorMessage: '상품 삭제에 실패하였습니다.' });
   }
 });
 
--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -1,22 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const ProductSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: true,
+const ProductSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    authorName: { type: String, required: true },
+
+    password: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ['FOR_SALE', 'SOLD_OUT'],
+      default: 'FOR_SALE',
+    },
   },
+  {
+    timestamps: true,
+  }
+);
 
-  content: {
-    type: String,
-    required: true,
-  },
-
-  authorName: { type: String, required: true },
-
-  password: { type: String, required: true },
-  status: { type: String, enum: ["FOR_SALE", "SOLD_OUT"], default: "FOR_SALE" },
-
-  timestamps: true,
-});
-
-export default mongoose.model("Product", ProductSchema);
+export default mongoose.model('Product', ProductSchema);
